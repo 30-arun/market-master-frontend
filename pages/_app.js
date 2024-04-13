c42e0b4fcb8e3aeb6ff17445428542a250e789c3
Arun <maskedman9817@gmail.com>
2024-04-13 16:23:09 +0500

    fixed favicon

--- pages/_app.js
+++ pages/_app.js
@@ -20,23 +20,7 @@ function MyApp({ Component, pageProps }) {
 								<link
 									rel="shortcut icon"
 									href="/logos/favicon.ico"
-								/>
-								<link
-									rel="apple-touch-icon"
-									sizes="180x180"
-									href="/logos/apple-touch-icon.png"
-								/>
-								<link
-									rel="icon"
-									type="image/png"
-									sizes="32x32"
-									href="/logos/favicon-32x32.png"
-								/>
-								<link
-									rel="icon"
-									type="image/png"
-									sizes="16x16"
-									href="/logos/favicon-16x16.png"
+									type="image/x-icon"
 								/>
 								<link
 									rel="stylesheet"
