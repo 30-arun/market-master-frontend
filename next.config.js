4f3ff77a16198ac592abd1c957d34816c28bb502
Arun <maskedman9817@gmail.com>
2024-04-16 12:06:32 +0500

    fixed config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 // next.config.js
 module.exports = {
   // Use the full path if your Django app is not at the root
-  assetsPrefix: process.env.NODE_ENV === "production" ? "/static" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/static/" : "",
 
 };
