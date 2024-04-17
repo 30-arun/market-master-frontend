00d0dea54d7193cd04c87a63705ca3c47559b8d3
Arun <maskedman9817@gmail.com>
2024-04-18 02:24:45 +0500

    fixed homepage

--- pages/index.js
+++ pages/index.js
@@ -45,9 +45,8 @@ export default function Index() {
 		);
 	}
 
-	// // if subdomain ends with string ".com" then it is a top level domain
-  if (subdomain === hostName && !ecommerce)
-    return <Home/>
+  // if (subdomain === hostName && !ecommerce)
+  //   return <Home/>
 	if (!domain.endsWith(hostName)) {
 		if (ecommerce) return <Ecommerce />;
 		return <UserWebsite />;
