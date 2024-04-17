18e21ca141ba0ed78307652a77f8a684d919b04b
Arun <maskedman9817@gmail.com>
2024-04-18 02:22:23 +0500

    Revert "fixed home page"
    This reverts commit d7ad17192230f40c037b55fa92161d21990fbfab.

--- pages/index.js
+++ pages/index.js
@@ -7,66 +7,59 @@ import Ecommerce from "../components/EcommerceUser";
 const hostName = process.env.NEXT_PUBLIC_HOST_NAME;
 
 export default function Index() {
-  const [ecommerce, setEcommerce] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const domain = window.location.hostname.replace("www.", "");
-  const subdomain = window.location.hostname.split(".")[0];
+	const [ecommerce, setEcommerce] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const domain = window.location.hostname.replace("www.", "");
+	const subdomain = window.location.hostname.split(".")[0];
 
-  useEffect(() => {
-    if (subdomain !== hostName) {
-      // Avoid fetchData call on localhost
-      const fetchData = async () => {
-        setLoading(true);
-        try {
-          const response = await axios.get(
-            `${process.env.NEXT_PUBLIC_API_ROUTE_NAME}/store/slug_id/${subdomain}/`
-          );
-          console.log(response.data);
-          setEcommerce(response.data.ecommerce);
-        } catch (error) {
-          console.log(error);
-        } finally {
-          setLoading(false);
-        }
-      };
-      fetchData();
-    } else {
-      console.log("Localhost");
-    }
-  }, [subdomain]);
+	useEffect(() => {
+		if (subdomain !== hostName) {
+			// Avoid fetchData call on localhost
+			const fetchData = async () => {
+				setLoading(true);
+				try {
+					const response = await axios.get(
+						`${process.env.NEXT_PUBLIC_API_ROUTE_NAME}/store/slug_id/${subdomain}/`
+					);
+					console.log(response.data);
+					setEcommerce(response.data.ecommerce);
+				} catch (error) {
+					console.log(error);
+				} finally {
+					setLoading(false);
+				}
+			};
+			fetchData();
+		} else {
+			console.log("Localhost");
+		}
+	}, [subdomain]);
 
-  if (loading) {
-    return (
-      <div class="d-flex justify-content-center">
-        <div class="spinner-grow" role="status">
-          <span class="visually-hidden">Loading...</span>
-        </div>
-      </div>
-    );
-  }
+	if (loading) {
+		return (
+			<div class="d-flex justify-content-center">
+				<div class="spinner-grow" role="status">
+					<span class="visually-hidden">Loading...</span>
+				</div>
+			</div>
+		);
+	}
 
-  // if (domain.includes(hostName)) {
-  //   if (subdomain === hostName) return <Home />;
-  //   if (ecommerce) return <Ecommerce />;
-  //   return <UserWebsite />;
-  // }
-  // if (!domain.includes(hostName)) {
-  //   return <UserWebsite />;
-  // }
+	// // if subdomain ends with string ".com" then it is a top level domain
+  if (subdomain === hostName && !ecommerce)
+    return <Home/>
+	if (!domain.endsWith(hostName)) {
+		if (ecommerce) return <Ecommerce />;
+		return <UserWebsite />;
+	}
 
-  if (subdomain === hostName) return <Home />;
-  // if (!domain.endsWith(hostName)) {
-  //   if (ecommerce) return <Ecommerce />;
-  //   return <UserWebsite />;
-  // }
-
-  return (
-    <>
-      {ecommerce ? (
-        <Ecommerce />
-      ) : (
-        <>{subdomain === hostName ? <Home /> : <UserWebsite />}</>
-      )}
-    </>
-  );
+	return (
+		<>
+			{ecommerce ? (
+				<Ecommerce />
+			) : (
+				<>{subdomain === hostName ? <Home /> : <UserWebsite />}</>
+			)}
+		</>
+	);
 }
