c935a66ea9ed3181605c7f700d6644786927c55b
Arun <maskedman9817@gmail.com>
2024-04-20 15:37:35 +0500

    updated config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,8 @@ module.exports = {
         {
             name: "next-app",
             script: "server.js",
-            watch: ["server.js", ".next"],
-            ignore_watch: ["node_modules", "logs"],
+            // watch: ["server.js", ".next"],
+            // ignore_watch: ["node_modules", "logs"],
             log_date_format: "YYYY-MM-DD HH:mm Z",
             out_file: "/var/log/next-app-out.log",
             error_file: "/var/log/next-app-error.log",
